a9a4a7c15c70ad7040934b3ea30ffe9d27eff1d9
PSGtatitos <tatitoschris@gmail.com>
2025-05-18 03:09:28 +0300

    Update c-to-f.py

--- c-to-f.py
+++ c-to-f.py
@@ -2,11 +2,11 @@ this = input("Do you want to convert Celsius to Fahrenheit(1) or Fahrenheit to C
 this = int(this)
 if this == 1:
         degrees = input("How many degrees Celsius do you want to convert to Fahrenheit? ")
-        degrees = int(degrees)
+        degrees = float(degrees)
         fahrenheit = degrees * 1.8 + 32
         print(fahrenheit)
 elif this == 2:
         degrees1 = input("How many degrees Fahrenheit do you want to convert to Celsius? ")
-        degrees1 = int(degrees1)
+        degrees1 = float(degrees1)
         celcius = degrees1 * 1.8 + 32
         print(celcius)
